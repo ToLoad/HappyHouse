dea36eac7508b739d739d9f0c297e2946a068908
dbsgnl1021@naver.com <dbsgnl1021@naver.com>
2021-11-24 09:44:49 +0900

    접근차단

--- FrontEnd/happyhouse_final/src/router/index.js
+++ FrontEnd/happyhouse_final/src/router/index.js
@@ -22,8 +22,26 @@ import QuestionWrite from "@/components/QnA/QuestionWrite.vue";
 import QuestionView from "@/components/QnA/QuestionView.vue";
 import QuestionUpdate from "@/components/QnA/QuestionUpdate.vue";
 
-Vue.use(VueRouter);
+import store from "@/store/index.js";
 
+Vue.use(VueRouter);
+const onlyAuthUser = async (to, from, next) => {
+  // console.log(store);
+  const checkUserInfo = store.getters["memberStore/checkUserInfo"];
+  const getUserInfo = store._actions["memberStore/getUserInfo"];
+  let token = sessionStorage.getItem("access-token");
+  if (checkUserInfo == null && token) {
+    await getUserInfo(token);
+  }
+  if (checkUserInfo === null) {
+    alert("로그인이 필요한 페이지입니다..");
+    // next({ name: "SignIn" });
+    router.push({ name: "SignIn" });
+  } else {
+    console.log("로그인 했다.");
+    next();
+  }
+};
 const routes = [
   {
     path: "/",
@@ -53,6 +71,7 @@ const routes = [
       {
         path: "mypageupdate",
         name: "MyPageUpdate",
+        beforeEnter: onlyAuthUser,
         component: MyPageUpdate,
       },
     ],
@@ -60,11 +79,13 @@ const routes = [
   {
     path: "/house",
     name: "House",
+    beforeEnter: onlyAuthUser,
     component: House,
   },
   {
     path: "/notice",
     name: "Notice",
+    beforeEnter: onlyAuthUser,
     component: Notice,
     children: [
       {
@@ -92,6 +113,7 @@ const routes = [
   {
     path: "/qna",
     name: "QnA",
+    beforeEnter: onlyAuthUser,
     component: QnA,
     children: [
       {
